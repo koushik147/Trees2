dc7ee478b8cca67949ebefd68203606a1cb9a704
Koushik <55914356+koushik147@users.noreply.github.com>
2022-10-23 20:04:23 -0400

    Add files via upload

---
+++ Trees2/PathSum2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        
+        self.target = targetSum # creating target element
+        self.result = [] # creating resultant array
+        self.recur(root,0,[]) # calling recur function
+        return self.result # returning result
+    
+    def recur(self,root,currSum,path):
+        #base condition
+        if root == None:
+            return
+        
+        currSum = currSum + root.val # calculating current sum
+        path.append(root.val) # appending root value in path
+        
+        self.recur(root.left,currSum,path) # calling left side of root 
+        
+        if root.left == None and root.right == None:
+            if currSum == self.target: # if current sum is equal to target
+                temp = path[:] # copying to temp
+                self.result.append(temp) # appending temp to resultant array
+        
+        self.recur(root.right,currSum,path) # calling right side of root
+        
+        path.pop() # poppping the path
---
+++ Trees2/SumRootToLeafNumbers.py
@@ -0,0 +1,18 @@
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        
+        self.result = 0 # initializing result with 0
+        self.inorderTraversal(root,0) # calling inorder function
+        return self.result # returning result
+    
+    def inorderTraversal(self,root,currSum):
+        #base condition
+        if root == None:
+            return
+        
+        self.inorderTraversal(root.left,currSum*10+root.val) # calling left side of root and calculating the current sum
+        
+        if root.left == None and root.right == None:
+            self.result += currSum*10+root.val # adding the current value in result
+            
+        self.inorderTraversal(root.right,currSum*10+root.val) # calling left side of root and calculating the current sum
